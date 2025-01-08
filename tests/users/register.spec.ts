881316b80f8b3dd46e5aa7f126973c786521211f
cSahu14 <sahuchidananda1999@gmail.com>
2025-01-08 11:11:25 +0530

    add test for persist user

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,6 +1,22 @@
 import request from 'supertest'
 import app from '../../src/app'
+import { DataSource } from 'typeorm'
+import { AppDataSource } from '../../src/config/data-source'
+import { truncateTables } from '../utils'
 describe('POST auth/register', () => {
+    let connection: DataSource
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize()
+    })
+
+    beforeEach(async () => {
+        // database truncate
+        await truncateTables(connection)
+    })
+
+    afterAll(async () => {
+        await connection.destroy()
+    })
     describe('Given all fields', () => {
         it('Should return the 201 status code', async () => {
             // Arrange
@@ -9,14 +25,13 @@ describe('POST auth/register', () => {
                 firstName: 'Chidananda',
                 lastName: 'Sahu',
                 email: 'sahuchidananda1999@gmail.com',
-                password: 'admin',
+                password: 'secret',
             }
             // Act
             const response = await request(app)
                 .post('/auth/register')
                 .send(userData)
             // Assert
-
             expect(response.statusCode).toBe(201)
         })
         it('Should return valid json response', async () => {
@@ -51,6 +66,12 @@ describe('POST auth/register', () => {
             // Act
             await request(app).post('/auth/register').send(userData)
             // Assert
+            const userRepository = connection.getRepository('user')
+            const users = await userRepository.find()
+            expect(users).toHaveLength(1)
+            expect(users[0].firstName).toBe(userData.firstName)
+            expect(users[0].lastName).toBe(userData.lastName)
+            expect(users[0].email).toBe(userData.email)
         })
     })
     describe('/Fields are missing.', () => {})

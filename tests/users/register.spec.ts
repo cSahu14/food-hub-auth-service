aec88f796f6824f2e4248ce71248666eb92dd393
cSahu14 <sahuchidananda1999@gmail.com>
2025-01-11 19:52:22 +0530

    add test for unique email

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -3,6 +3,7 @@ import app from '../../src/app'
 import { DataSource } from 'typeorm'
 import { AppDataSource } from '../../src/config/data-source'
 import { Roles } from '../../src/constants'
+import { User } from '../../src/entity/User'
 describe('POST auth/register', () => {
     let connection: DataSource
     beforeAll(async () => {
@@ -66,7 +67,7 @@ describe('POST auth/register', () => {
             // Act
             await request(app).post('/auth/register').send(userData)
             // Assert
-            const userRepository = connection.getRepository('user')
+            const userRepository = connection.getRepository(User)
             const users = await userRepository.find()
             expect(users).toHaveLength(1)
             expect(users[0].firstName).toBe(userData.firstName)
@@ -88,7 +89,7 @@ describe('POST auth/register', () => {
 
             // Assert
 
-            const userRepository = connection.getRepository('user')
+            const userRepository = connection.getRepository(User)
             const users = await userRepository.find()
             expect(users[0]).toHaveProperty('id')
         })
@@ -108,10 +109,56 @@ describe('POST auth/register', () => {
 
             // Assert
 
-            const userRepository = connection.getRepository('user')
+            const userRepository = connection.getRepository(User)
             const users = await userRepository.find()
             expect(users[0]).toHaveProperty('role')
             expect(users[0].role).toBe(Roles.CUSTOMER)
+        })
+
+        it('Should store the hashed password in database.', async () => {
+            // Arrange
+
+            const userData = {
+                firstName: 'Chidananda',
+                lastName: 'Sahu',
+                email: 'sahuchidananda1999@gmail.com',
+                password: 'admin',
+            }
+
+            // Act
+            await request(app).post('/auth/register').send(userData)
+
+            // Assert
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            expect(users[0].password).not.toBe(userData.password)
+            expect(users[0].password).toHaveLength(60)
+            expect(users[0].password).toMatch(/^\$2b\$\d+\$/)
+        })
+
+        it('Should return 400 status code if email is alreay exist.', async () => {
+            // Arrange
+
+            const userData = {
+                firstName: 'Chidananda',
+                lastName: 'Sahu',
+                email: 'sahuchidananda1999@gmail.com',
+                password: 'admin',
+            }
+
+            const userRepository = connection.getRepository(User)
+            await userRepository.save({ ...userData, role: Roles.CUSTOMER })
+
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            const users = await userRepository.find()
+
+            // Assert
+            expect(response.statusCode).toBe(400)
+            expect(users).toHaveLength(1)
         })
     })
     describe('/Fields are missing.', () => {})
